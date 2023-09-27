bcc2db0af704a1d9223036f992545f1ca45a274e
Marisa Goergen <mgoergen@phoenixcontact.com>
2023-09-27 09:27:53 +0200

    Fix configurations

--- WebApplication6/Program.cs
+++ WebApplication6/Program.cs
@@ -1,13 +1,17 @@
 using Microsoft.AspNetCore.Authorization;
 using Moryx.Identity.AccessManagement;
+using Moryx.Launcher;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
 
 
 // Add services to the container.
 builder.Services.AddMoryxAccessManagement(
             builder.Configuration.GetSection("Jwt"),
-            builder.Configuration.GetConnectionString("ConnectionString"), // e.g. "Username=postgres;Password=postgres;Host=localhost;Port=5432;Persist Security Info=True;Database=IdentityServer"
+            builder.Configuration.GetConnectionString("DefaultConnection"), // e.g. "Username=postgres;Password=postgres;Host=localhost;Port=5432;Persist Security Info=True;Database=IdentityServer"
             corsOptions =>
             {
                 corsOptions.AddPolicy("CorsPolicy", builder => builder
@@ -16,8 +20,15 @@ builder.Services.AddMoryxAccessManagement(
                 .AllowAnyHeader()
                 .AllowCredentials());
             });
+builder.Services.AddSingleton<IShellNavigator, ShellNavigator>();
+builder.Services.AddRazorPages();
 
 var app = builder.Build();
+
+
+await app.Migrate().Seed();
+
+
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
@@ -29,6 +40,9 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 //app.MapGet("/", () => "Hello World!");
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
 app.Run();
